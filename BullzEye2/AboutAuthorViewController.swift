fc91bafafe6781ed4d74268aa49e8205b0ffd990
Admin <admin@admins-macbook-pro.local>
2018-01-28 12:41:08 -0800

    Completed without extra graphics.

---
+++ BullzEye2/AboutAuthorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutAuthorViewController.swift
+//  BullzEye2
+//
+//  Created by Admin on 1/28/18.
+//  Copyright © 2018 Matthew Heinrichs. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
